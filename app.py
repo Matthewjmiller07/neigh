e7efc64f5e97c398b3455fa0cd4a334c35263823
Matthew Miller <Matthewjmiller07@gmail.com>
2023-10-04 18:13:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
 from flask import Flask, request, jsonify
 import requests
-import json
 
 app = Flask(__name__)
 
 @app.route('/get_neighborhood', methods=['GET'])
 def get_neighborhood_from_geonames():
-    lat = request.args.get('lat')
-    lon = request.args.get('lon')
-    username = "matthewjmiller07"  # Your GeoNames username
-    
-    base_url = "http://api.geonames.org/neighbourhoodJSON?"
-    params = {
-        "lat": lat,
-        "lng": lon,
-        "username": username
-    }
-    
-    response = requests.get(base_url, params=params)
-    
-    if response.status_code != 200:
-        return jsonify({"Error": response.status_code})
-    
-    data = json.loads(response.text)
-    
-    if 'neighbourhood' in data:
-        neighborhood_info = data['neighbourhood']
-        return jsonify({"neighborhood": neighborhood_info.get('name', "Not available")})
-    else:
-        return jsonify({"neighborhood": "Not available"})
+    try:
+        lat = request.args.get('lat')
+        lon = request.args.get('lon')
+        username = "matthewjmiller07"  # Your GeoNames username
+        
+        base_url = "http://api.geonames.org/neighbourhoodJSON?"
+        params = {
+            "lat": lat,
+            "lng": lon,
+            "username": username
+        }
+        
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+        
+        data = response.json()  # Parse JSON
+        
+        if 'neighbourhood' in data:
+            neighborhood_info = data['neighbourhood']
+            return jsonify({"neighborhood": neighborhood_info.get('name', "Not available")})
+        else:
+            return jsonify({"neighborhood": "Not available"})
+    except requests.RequestException as e:
+        return jsonify({"Error": str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
