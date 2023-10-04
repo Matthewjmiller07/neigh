ea6600e305365a5c4375ca70146764f73d9d08d2
Matthew Miller <Matthewjmiller07@gmail.com>
2023-10-04 08:08:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def get_neighborhood_from_geonames():
         return jsonify({"neighborhood": "Not available"})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
