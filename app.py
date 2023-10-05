160e4c549d688953b5e4d2bd5aa8d4d1b63b7ad4
Matthew Miller <Matthewjmiller07@gmail.com>
2023-10-05 16:10:37 +0100

    Update app.py
    added wikipedia data

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, jsonify
 import requests
+import wikipediaapi
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+wiki_wiki = wikipediaapi.Wikipedia(language='en', user_agent='my_application')
 
 app = Flask(__name__)
 
@@ -9,7 +15,12 @@ def get_neighborhood_from_geonames():
         lat = request.args.get('lat')
         lon = request.args.get('lon')
         username = "matthewjmiller07"  # Your GeoNames username
-        
+
+        logging.info(f"Received request for lat: {lat}, lon: {lon}")
+
+        # Initialize Wikipedia API
+        wiki_wiki = wikipediaapi.Wikipedia(language='en', user_agent='my_application')
+
         # First API call for neighbourhood information
         base_url = "http://api.geonames.org/neighbourhoodJSON?"
         params = {
@@ -17,17 +28,19 @@ def get_neighborhood_from_geonames():
             "lng": lon,
             "username": username
         }
-        
+
         response = requests.get(base_url, params=params)
-        response.raise_for_status()  # Raise HTTPError for bad responses
-        
-        data = response.json()  # Parse JSON
-        
+        response.raise_for_status()
+        data = response.json()
+
+        logging.info(f"GeoNames API full response: {data}")
+
+        search_term = None
+
         if 'neighbourhood' in data:
-            neighborhood_info = data['neighbourhood']
-            return jsonify({"neighborhood": neighborhood_info.get('name', "Not available")})
+            search_term = data['neighbourhood'].get('name', "Not available")
         elif 'city' in data:
-            return jsonify({"neighborhood": data.get('city', "Not available")})
+            search_term = data.get('city', "Not available")
         else:
             # Fallback API call for place name information
             fallback_url = "http://api.geonames.org/findNearbyPlaceNameJSON?"
@@ -43,11 +56,24 @@ def get_neighborhood_from_geonames():
             fallback_data = fallback_response.json()
             
             if 'geonames' in fallback_data and len(fallback_data['geonames']) > 0:
-                return jsonify({"neighborhood": fallback_data['geonames'][0].get('name', "Not available")})
+                search_term = fallback_data['geonames'][0].get('name', "Not available")
+
+        wikipedia_info = None
+
+        logging.info(f"Searching Wikipedia for: {search_term}")
+
+        if search_term:
+            page = wiki_wiki.page(search_term)
+            if page.exists():
+                wikipedia_info = page.summary[:1000]
+                logging.info(f"Wikipedia page found: {wikipedia_info[:100]}")
             else:
-                return jsonify({"neighborhood": "We don't have info"})
-                
+                logging.info("Wikipedia page does not exist.")
+
+        return jsonify({"neighborhood": search_term, "wikipedia_info": wikipedia_info})
+
     except requests.RequestException as e:
+        logging.error(f"Request error: {e}")
         return jsonify({"Error": str(e)})
 
 if __name__ == '__main__':
